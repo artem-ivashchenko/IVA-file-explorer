7215135d3fe3dd83f0dc39441ea610447dd29520
Artem <artem.ivashchenko.exe@gmail.com>
2024-01-06 19:22:48 +0200

    update deploy

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,16 @@
-// config-overrides.js
 const crypto = require('crypto-browserify');
 const util = require('util');
 
-module.exports = function override(config) {
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    crypto: false, 
-    util: false,
-  };
+const isEnvProduction = process.env.NODE_ENV === 'production';
+
+module.exports = function override(config, env) {
+  if (isEnvProduction) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: false,
+      util: false,
+    };
+  }
 
   return config;
 };
